986bf85ba7a4f0a7cce93d8e0f46f431c6a91673
emiliesedziol <sedziole@gmail.com>
2018-07-28 13:43:31 -0400

    intial QT commit

---
+++ 1-4Console/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    cout << "What does this do" << endl;
+    // cout - print to console
+    // line return
+    return 0;
+}
