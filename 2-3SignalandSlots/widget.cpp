58ae3e9298c5d3b46a5ba3350bcd378707cca0f0
emiliesedziol <sedziole@gmail.com>
2018-07-31 10:50:59 -0400

    Signals and slots

---
+++ 2-3SignalandSlots/widget.cpp
@@ -0,0 +1,38 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    /*
+    1 - String Notation
+    connect(ui->pushButtonAA,SIGNAL(clicked()),
+            this,SLOT(changeText()));
+    */
+/*
+    2 - Functor Notation : Regular Slots
+    connect(ui->pushButtonAA,&QPushButton::clicked,this,&Widget::changeText);
+    */
+
+    // 3 - Functor Notation lambdas
+    connect(ui->pushButtonAA,&QPushButton::clicked,[=](){
+        ui->labelBB->setText("Lamdba functor notation");
+    });
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::changeText()
+{
+    qDebug() << "the button was pushed";
+
+    ui->labelBB->setText("The button has been clicked");
+}
