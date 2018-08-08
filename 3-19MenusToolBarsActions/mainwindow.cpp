4fa112c787d3984ca1da5888c81a8206568ce944
emiliesedziol <sedziole@gmail.com>
2018-08-08 12:31:55 -0400

    adding the actions

--- 3-19MenusToolBarsActions/mainwindow.cpp
+++ 3-19MenusToolBarsActions/mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QApplication>
+#include <QDebug>
+#include <QMessageBox>
+#include <QTimer>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +15,52 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionQuit_triggered()
+{
+   statusBar()->showMessage("App will shut down in 5 secons");
+   QTimer::singleShot(5000, this,SLOT(quitApp()));
+   // QApplication::quit();
+   // qDebug() << "Quit action";
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->textEdit->copy();
+}
+
+void MainWindow::on_actionCut_triggered()
+{
+    ui->textEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->textEdit->paste();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->textEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->textEdit->redo();
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    QMessageBox::about(this,"Message","This is an exercise on how to use menus toolbars in actions in Qt");
+}
+
+void MainWindow::on_actionAbout_Qt_triggered()
+{
+    QApplication::aboutQt();
+}
+
+void MainWindow::quitApp()
+{
+    QApplication::quit();
 }
