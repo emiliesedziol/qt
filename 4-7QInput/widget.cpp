7c4b070bb01a133bd81041807d9a5a92b8ba8443
emiliesedziol <sedziole@gmail.com>
2018-08-09 13:26:38 -0400

    Input

---
+++ 4-7QInput/widget.cpp
@@ -0,0 +1,40 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QInputDialog>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    QStringList items;
+    items << tr("Spring") << tr("Summer") << tr("Fall") << tr("Winter");
+
+    bool ok;
+    QString item = QInputDialog::getItem(this, tr("QInputDialog::getItem()"),
+                                         tr("Season:"), items, 0, false, &ok);
+    if (ok && !item.isEmpty())
+    {
+        qDebug() << item << " was selected";
+    }
+
+    /*
+    bool ok;
+    double d = QInputDialog::getDouble(this, tr("QInputDialog::getDouble()"),
+                                       tr("Amount:"), 37.56, -10000, 10000, 2, &ok);
+    if (ok)
+    {
+        qDebug() << "Double is " << QString::number(d);
+    }
+    */
+}
